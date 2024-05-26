dae988c319693b3e28741ad1c97b61112fcac250
EdwardC22 <111481828+EdwardC22@users.noreply.github.com>
2024-05-26 13:20:30 +0900

    Delete capstone-project-9900-t16p-undefined-behaviour-main directory

--- capstone-project-9900-t16p-undefined-behaviour-main/config.sh
+++
@@ -1,8 +0,0 @@
-export DB_USERNAME="backend"
-export DB_PASSWORD="password"
-export JWT_SECRET_KEY="Secret-Key"
-
-export DUMMY_USERNAME="username"
-export DUMMY_PASSWORD="password"
-export DUMMY_EMAIL="email@email.com"
-export DUMMY_RESTAURANT="Restaurant"
